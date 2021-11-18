d5eee50ec74a2b6869af8224add1090e5afdbad9
inFlowia <66429322+inFlowia@users.noreply.github.com>
2021-11-18 17:53:19 +0300

    Update deps.js

--- deps.js
+++ deps.js
@@ -42,9 +42,8 @@ function get_this_dir_url(doc_cur_script){
 /* подключает скрипт с заданным в аргументе путём и именем в виде /someCat/someScrpt.js из каталога скрипта
 АРГУМЕНТЫ:
 	path - путь к скрипту в виде /someCat/someScrpt.js
-	is_async - не обязательный
+	is_async - не обязательный ВНИМАНИЕ! Не работает так как может ожидаться - то есть если в скрипте есть подлючение скрипта с is_async = false это не значит, что скрипт подождёт пока он подключится. нет, он сразу продолжит выполнение после подключения а подключенный скрипт подключится всё равно потом.
 	to_head - добавлять ли скрипт в head (true - в head, false - в body) не обязательный
-НЕ ЗАБЫВАЙ! Подключение произойдёт после (document).ready!
 Если подкл. модуль используется в JS в основном документе то лучше не рисковать и использование выполнять с зажержкой после document ready:
 $(document).ready(function(){setTimeout(showRndCh1, 4000);});
 function showRndCh1(){
@@ -52,13 +51,15 @@ function showRndCh1(){
 };
 */
 function include_script(path, is_async = true, to_head = true){
-    let script = document.createElement('script');
-    script.src  = path;
-		if(to_head)
-    	document.head.appendChild(script);
-		else
-			document.body.appendChild(script);
+	let script = document.createElement('script');
+	script.src = path;
+	script.async = is_async;
+	if(to_head)
+		document.head.appendChild(script);
+	else
+		document.body.appendChild(script);
 }
+
 
 
 /* подключает стиль
